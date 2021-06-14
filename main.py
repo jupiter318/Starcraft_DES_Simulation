765dd6925122a9101b0f720113b973ea7d54e1bd
Lee DY <doyeop.lee@snu.ac.kr>
2021-06-15 00:51:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@
 # 시간 단위는 Normal Gamespeed 기준에서 second 단위.
 # Fastest 는 30% 빠름
 
-
-import simpy
 import pandas as pd
+import simpy
+import csv
 
 ######
 # initial parameters
@@ -140,54 +140,86 @@ def mineral_mining(env, mineral_container, command_center):
     while True:
         yield env.timeout(1)
         yield mineral_container.put(mining_rate(command_center.scv_number))
-        print(f"{env.now}:\t"
-              f"current mineral\t{mineral_container.level}\t"
-              f"scv_number\t{command_center.scv_number}\t"
-              f"barracks_number\t{command_center.barracks_list.__len__()}\t"
-              f"total_marine_number\t{current_marine_number(command_center.barracks_list)}\t"
-              f"marine_production_number\t{[x.marine_number for x in command_center.barracks_list]}\t"
-              f"barracks_end_time\t{[x.end_time for x in command_center.barracks_list]}",
-              status_msg)
+        # print(f"{env.now}:\t"
+        #       f"current mineral\t{mineral_container.level}\t"
+        #       f"scv_number\t{command_center.scv_number}\t"
+        #       f"barracks_number\t{command_center.barracks_list.__len__()}\t"
+        #       f"total_marine_number\t{current_marine_number(command_center.barracks_list)}\t"
+        #       f"marine_production_number\t{[x.marine_number for x in command_center.barracks_list]}\t"
+        #       f"barracks_end_time\t{[x.end_time for x in command_center.barracks_list]}",
+        #       status_msg)
         status_msg = ""
 
 
-def setup(env, input_string):
-    global status_msg
+def setup(env, order_string):
+    global status_msg, end_production_time
     mineral_container = simpy.Container(env=env, init=50)
     cmd_center = CommandCenter(env=env, mineral_container=mineral_container)
     env.process(mineral_mining(env=env, mineral_container=mineral_container, command_center=cmd_center))
-    
-    for next_product in input_string:
-        if next_product == 'S': # Add SCV
+
+    for next_product in order_string:
+        if next_product == 's': # Add SCV
             yield mineral_container.get(50)
             env.process(build_scv(cmd_center))
-        elif next_product == 'B':   # Add Barracks
+        elif next_product == 'b':   # Add Barracks
             yield mineral_container.get(150)
             env.process(build_barracks(cmd_center))
-        elif next_product == 'M':   # Add Marine
+        elif next_product == 'm':   # Add Marine
             while cmd_center.barracks_list.__len__() == 0:      # wait until first barracks
                 yield env.timeout(1)
             yield mineral_container.get(50)
             end_time_list = [x.end_time for x in cmd_center.barracks_list]
+            end_production_time = min(end_time_list)+marine_build_time
             status_msg += f'\torder to {end_time_list.index(min(end_time_list))}'
             env.process(build_marine(cmd_center.barracks_list[end_time_list.index(min(end_time_list))]))
-            # end_time이 가장 작은 배럭에 주문 추가
+    print(order_string, f"\tend time:{end_production_time}\t")
+
+        # end_time이 가장 작은 배럭에 주문 추가
         # elif next_product == 'U': # Add Supply Depot
         #     yield mineral_container.get(100)
         #     env.process(build_supply(cmd_center))
 
 
-SIM_TIME = 600
-env = simpy.Environment()
-input_string = "SSBMMMBBMMMMBBMMMMMMMMMMM"
-env.process(setup(env, input_string))
-env.run(until=SIM_TIME)
+SIM_TIME = 60*60
+end_production_time = 10*60
+# env = simpy.Environment()
+# input_string = "SSBMMMBBMMMMBBMMMMMMBMMMBBMMMMBBMMMMMMBMMMBBMMMMBBMMMMMMBMMMBBMMMMBBMMMMMMBMMMBBMMMMBBMMMMMMBMMMBBMMMMBBMMMMMMMMMMM"
+# env.process(setup(env, input_string))
+# env.run(until=SIM_TIME)
+orders = pd.read_csv('m12b2s4_full.csv')
+
+for i in orders['order']:
+    env = simpy.Environment()
+    env.process(setup(env, i))
+    env.run(until=1.5*end_production_time)
 
 
 
-# order = pd.read_csv('project.CSV')
 
-# print(order)
+
+#
+# with open('m12b2s4.csv', 'r') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         if format(*row).__len__() > 5:
+#             env = simpy.Environment()
+#             env.process(setup(env, ))
+#             env.run(until=1.5*end_production_time)
+#
+#
+#
+# # list of name, degree, score
+# nme = ["aparna", "pankaj", "sudhir", "Geeku"]
+# deg = ["MBA", "BCA", "M.Tech", "MBA"]
+# scr = [90, 40, 80, 98]
+#
+# # dictionary of lists
+# dict = {'order': order_string, 'end_time': end_production_time, 'score': scr}
+#
+# df = pd.DataFrame(dict)
+#
+# # saving the dataframe
+# df.to_csv(r'C:\Users\Admin\Desktop\file3.csv', index=False)
 #
 # for i in order:
 #     env = simpy.Environment()
